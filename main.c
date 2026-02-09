1485abf4c071bd8b36785dd7caa1785924474672
albertoDev <devalberto98@gmail.com>
2026-02-09 14:01:38 +0100

    added ecu and ecu state

--- main.c
+++ main.c
@@ -2,10 +2,18 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+typedef enum { ERROR_ACTIVE, ERROR_PASSIVE, BUS_OFF } ECUState;
+
+typedef struct ECU {
+    ECUState state; 
+    bool is_transmitting;
+} ECU;
+
 typedef struct CANBus {
     ECU **nodes; /* connected ecus */
     uint8_t node_count;
     bool is_idle;
+    bool collision_detected; /* if there is a collision in the currect bus tick */
 } CANBus;
 
 int main() {
