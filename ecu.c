7c63705f1edb9b9e6bfb4e8e5ab834cdb9554e13
albertoDev <devalberto98@gmail.com>
2026-02-10 17:51:18 +0100

    refactoring check_transmission_outcome func

--- ecu.c
+++ ecu.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 #include "ecu.h"
 #include "frame.h"
@@ -56,4 +57,29 @@ void listen(ECU *ecu) {
     ecu->is_transmitting = false;
     ecu->current_msg = NULL;
 }
+
+void check_transmission_outcome(ECU *ecu) {
+    if (!ecu || !ecu->bus || !ecu->is_transmitting) return;
+
+    /* is winning message NULL? */
+    if (!ecu->bus->winning_msg) {
+        printf("This should never print if bus logic is correct!\n");
+        ecu->is_transmitting = false;
+        ecu->current_msg = NULL;
+        return;
+    }
+    
+    /* Did I win arbitration? */
+    if (ecu->current_msg->id == ecu->bus->winning_msg->id) {
+        /* Was there a collision? */
+        if (ecu->bus->collision_detected) {
+            ecu->tec += 8; /* transmission error */
+        } else { /* No collision */
+            if (ecu->tec > 0) ecu->tec--;
+        }
+    }
+    update_ecu_state(ecu);
+    ecu->is_transmitting = false;
+    ecu->current_msg = NULL;
+} 
 
